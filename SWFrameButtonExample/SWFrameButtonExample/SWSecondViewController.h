c517ef88091e7e3419617b0725ee330909b0cc60
Sarun Wongpatcharapakorn <artwork.th@gmail.com>
2015-04-28 10:47:13 +0700

    Add example for Vibrant Effect

---
+++ SWFrameButtonExample/SWFrameButtonExample/SWSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SWSecondViewController.h
+//  SWFrameButtonExample
+//
+//  Created by Sarun Wongpatcharapakorn on 4/28/15.
+//  Copyright (c) 2015 Sarun Wongpatcharapakorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWSecondViewController : UIViewController
+
+@end
